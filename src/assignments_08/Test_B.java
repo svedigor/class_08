346cc737ff6e05f944aa9c52696ca6bd7d832db4
Igor <sved.igor@gmail.com>
2020-05-18 15:14:05 +0300

    Test_B / 4

--- src/assignments_08/Test_B.java
+++ src/assignments_08/Test_B.java
@@ -1,9 +1,13 @@
 package assignments_08;
 
+import org.junit.AfterClass;
+import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
 
+import static java.lang.System.in;
 import static java.lang.System.out;
 
 public class Test_B extends MyActions{
@@ -11,6 +15,11 @@ public class Test_B extends MyActions{
     @BeforeClass
     public static void setDriver(){
         setChromeDriver();
+    }
+
+    @AfterClass
+    public static void quitDriver(){
+        driverChrome.quit();
     }
 
     @Test
@@ -26,5 +35,14 @@ public class Test_B extends MyActions{
     @Test
     public void test_3(){
         out.println("width of button '6' is " + driverChrome.findElement(By.id("six")).getSize().width+"px");
+    }
+
+    @Test
+    public void test_4(){
+        int myX = 843;
+        int myY = 163;
+        Point nineLocation = driverChrome.findElement(By.id("nine")).getLocation();
+        Assert.assertEquals(myX,nineLocation.x);
+        Assert.assertEquals(myY,nineLocation.y);
     }
 }
