8db7df093fba32bd5cf97213170308bca7f4b7c4
Igor <sved.igor@gmail.com>
2020-05-18 14:53:07 +0300

    Test_B / 2

--- src/assignments_08/Test_B.java
+++ src/assignments_08/Test_B.java
@@ -2,6 +2,9 @@ package assignments_08;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
+
+import static java.lang.System.out;
 
 public class Test_B extends MyActions{
 
@@ -13,5 +16,10 @@ public class Test_B extends MyActions{
     @Test
     public void test_1(){
         driverChrome.get("https://dgotlieb.github.io/WebCalculator/");
+    }
+
+    @Test
+    public void test_2(){
+        out.println(driverChrome.findElement(By.id("two")).getSize().height);
     }
 }
