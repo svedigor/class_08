f997b778e9012d0826d319045cb3b6e0e713ab68
Igor <sved.igor@gmail.com>
2020-05-19 17:05:01 +0300

    Test_E / - 3/4

--- src/assignments_08/Test_E.java
+++ src/assignments_08/Test_E.java
@@ -1,28 +1,30 @@
 package assignments_08;
 
 import com.example.JSUtils;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
-
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import static java.lang.System.out;
 
 public class Test_E extends MyActions{
+
+   protected Actions act = new Actions(driverChrome);
+
     @BeforeClass
     public static void setWebDriver(){
         setChromeDriver();
     }
 
-//    @AfterClass
-//    public static void quitWebDriver() throws InterruptedException {
-//        Thread.sleep(2000);
-//        driverChrome.quit();
-//    }
+    @AfterClass
+    public static void quitWebDriver(){
+        driverChrome.quit();
+    }
+
+    @After
+    public void waitAftreEachTest() throws InterruptedException {
+        Thread.sleep(1000);
+    }
 
     @Test
     public void test_1(){
@@ -30,9 +32,24 @@ public class Test_E extends MyActions{
     }
 
     @Test
-    public void test_2() throws InterruptedException {
+    public void test_2() {
         WebElement closeImg = driverChrome.findElement(By.xpath("//img[@id ='drag1']"));
         WebElement box = driverChrome.findElement(By.xpath("//div[@id = 'div1']"));
         JSUtils.JavascriptDragAndDrop(driverChrome,closeImg,box);
+    }
+
+    @Test
+    public void test_3(){
+        String expectedtext ="You double clicked";
+        WebElement elementToClick = driverChrome.findElement(By.xpath("//*[text() = 'Double-click text to trigger a function.']"));
+        act.doubleClick(elementToClick).perform();
+        String actualText = driverChrome.findElement(By.id("demo")).getText();
+        Assert.assertEquals(expectedtext,actualText);
+    }
+
+    @Test
+    public void test_4(){
+        WebElement closeImg = driverChrome.findElement(By.xpath("//img[@id ='close']"));
+        act.moveToElement(closeImg).perform();
     }
 }
